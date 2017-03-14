6e30c7c13d43b2991d11dfe77d2af35574e0410a
Galil Gertner <galilg@gmail.com>
2017-03-14 19:55:36 -0400

    import_entrez fixed.  Requires no pre-cleaning of the file, and puts it into csv format

--- import_entrez.py
+++ import_entrez.py
@@ -1,42 +1,57 @@
 #!/usr/bin/env
 
+#---- Imports -----------------------------------------------------------------
+
 import re
+
+#---- Functions ---------------------------------------------------------------
+
+def convert_data_to_csv_format(data):
+    csv_data =[]
+    for line in data:
+        if (is_begining_of_entrez_id(line)):
+            ids = line[0].split(" ")
+            gene_name = line[1].split('(')
+            ids[0] = ids[0].rstrip()
+            ids[1] = ids[-1].strip() + 'HUMAN'
+            gene_name[0] = gene_name[0].lstrip()
+            gene_name[0] = re.sub(r',', '', gene_name[0])
+            gene_name[1] = gene_name[1][:-2]
+            new_line = [ids[0], ids[1], gene_name[1], gene_name[0]]
+            csv_data.append(new_line)
+    return csv_data
+
 
 def is_begining_of_entrez_id(line):
     return bool(re.compile(r'[0-9]').match(line[0][:5]))
 
 
-data = []
-with open('../Entrez_copy', 'r') as f:
-    for line in f:
-        data.append(line.split("HUMAN"))
+def open_entrez_id_file(file):
+    data = []
+    with open(file, 'r') as f:
+        for line in f:
+            data.append(line.split("HUMAN"))
 
-header = ("entrez_id, uniprot_id, gene_name, description")
+    return data
 
-print("THE FREAKIN header: ", header)
 
-data = data[1:]
-new_data =[]
-for line in data:
-    if (is_begining_of_entrez_id(line)):
-        ids = line[0].split(" ")
-        gene_name = line[1].split('(')
-        ids[0] = ids[0].rstrip()
-        ids[1] = ids[-1].strip() + 'HUMAN'
-        gene_name[0] = gene_name[0].lstrip()
-        gene_name[1] = gene_name[1][:-2]
-        new_line = [ids[0], ids[1], gene_name[1], gene_name[0]]
-        new_data.append(new_line)
-
-print(new_data)
-with open('Entrez_id.csv', 'w') as outfile:
-    outfile.write(header)
-    outfile.write('\n')
-    for line in new_data:
-        for word in line:
-            if word == line[-1]:
-                outfile.write(word)
-            else:
-                outfile.write(word + ", ")
+def save_to_csv_file(cleaned_data):
+    with open('Entrez_id.csv', 'w') as outfile:
+        header = "entrez_id, uniprot_id, gene_name, description"
+        outfile.write(header)
         outfile.write('\n')
+        for line in cleaned_data:
+            for word in line:
+                if word == line[-1]:
+                    outfile.write(word)
+                else:
+                    outfile.write(word + ", ")
+            outfile.write('\n')
 
+
+#---- Main --------------------------------------------------------------------
+
+file_path = '../Entrez_copy'
+data = open_entrez_id_file(file_path)
+csv_data = convert_data_to_csv_format(data)
+save_to_csv_file(csv_data)
