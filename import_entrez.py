3fe3a7b7f14435c518d4c85069ccd77a471b95cc
Galil Gertner <galilg@gmail.com>
2017-03-14 19:27:28 -0400

    Import_entrez now working with raw input file. Next clean up file.

--- import_entrez.py
+++ import_entrez.py
@@ -2,8 +2,12 @@
 
 import re
 
+def is_begining_of_entrez_id(line):
+    return bool(re.compile(r'[0-9]').match(line[0][:5]))
+
+
 data = []
-with open('../Entrez_id_uniprot_mapping', 'r') as f:
+with open('../Entrez_copy', 'r') as f:
     for line in f:
         data.append(line.split("HUMAN"))
 
@@ -14,21 +18,20 @@ print("THE FREAKIN header: ", header)
 data = data[1:]
 new_data =[]
 for line in data:
-    ids = line[0].split(" ")
-    print("ids: ", ids)
-    gene_name = line[1].split('(')
-    ids[0] = ids[0].rstrip()
-    #print("Id 1: ", ids[1])
-    ids[1] = ids[-1].strip() + 'HUMAN'
-    #print("Id 2: ", ids[1])
-    gene_name[0] = gene_name[0].lstrip()
-    gene_name[1] = gene_name[1][:-2]
-    new_line = [ids[0], ids[1], gene_name[1], gene_name[0]]
-    print("The new line: ", new_line)
-    new_data.append(new_line)
+    if (is_begining_of_entrez_id(line)):
+        ids = line[0].split(" ")
+        gene_name = line[1].split('(')
+        ids[0] = ids[0].rstrip()
+        ids[1] = ids[-1].strip() + 'HUMAN'
+        gene_name[0] = gene_name[0].lstrip()
+        gene_name[1] = gene_name[1][:-2]
+        new_line = [ids[0], ids[1], gene_name[1], gene_name[0]]
+        new_data.append(new_line)
 
 print(new_data)
-with open('../Entrez_id.csv', 'w') as outfile:
+with open('Entrez_id.csv', 'w') as outfile:
+    outfile.write(header)
+    outfile.write('\n')
     for line in new_data:
         for word in line:
             if word == line[-1]:
